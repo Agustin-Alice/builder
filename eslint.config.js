b00b3e433b633e33afe9b4fac66594e92b57a71e
Agustin Alice <agustin.alice00@gmail.com>
2025-01-10 01:35:04 -0300

    Integrate Prettier with ESLint for unified formatting and linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 export default [
   {
@@ -14,20 +16,29 @@ export default [
     plugins: {
       react,
       'react-hooks': reactHooks,
+      prettier, // Agregar Prettier como plugin
     },
     rules: {
-      eqeqeq: 'error', // Requiere el uso de igualdad estricta (=== y !==) en lugar de == y !=
-      'no-console': 'warn', // Muestra una advertencia cuando se utiliza console.log()
-      'no-unused-vars': 'warn', // Muestra una advertencia cuando hay variables declaradas pero no utilizadas
-      semi: ['error', 'always'], // Requiere el uso de punto y coma al final de las declaraciones
-      quotes: ['error', 'single'], // Requiere el uso de comillas simples en lugar de dobles
-      indent: ['error', 2], // Requiere una indentación de 2 espacios
-      'no-magic-numbers': 'warn', // Muestra una advertencia cuando se utilizan números mágicos (números sin explicación)
-      'no-var': 'error', // Requiere el uso de let o const en lugar de var
-      'prefer-const': 'error', // Requiere el uso de const cuando una variable no es reasignada
-      'no-undef': 'error', // Previene el uso de variables no definidas
-      'no-trailing-spaces': 'error', // Previene espacios en blanco al final de las líneas
-      'comma-dangle': ['error', 'always-multiline'], // Requiere una coma al final de las listas de elementos
+      eqeqeq: 'error',
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'no-magic-numbers': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-undef': 'error',
+      'no-trailing-spaces': 'error',
+      'comma-dangle': ['error', 'always-multiline'],
+      'prettier/prettier': 'error',
     },
   },
-];
+
+  {
+    files: ['**/*.{js,jsx}'],
+    rules: {
+      ...prettierConfig.rules,
+    },
+  },
+]
